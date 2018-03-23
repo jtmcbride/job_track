16a2b183795a5ed8261bae8a92ddb889db653851
Julia Pavicic <jmcbriddle@gmail.com>
2018-03-22 18:09:03 -0700

    add job editing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import sqlite3 as sql
 app = Flask(__name__)
 
@@ -11,7 +11,7 @@ def index():
 	jobs = list(jobs)
 	return render_template('index.html', jobs=jobs)
 
-@app.route('/jobs', methods=['GET', 'POST'])
+@app.route('/jobs', methods=['GET', 'POST', 'PUT', 'PATCH'])
 def jobs():
 	if request.method == "POST":
 		comp = request.form['company']
@@ -24,4 +24,18 @@ def jobs():
 		c.close()
 
 
-	return render_template('index.html')
+	return redirect('/')
+
+@app.route('/jobs/<int:job_id>/edit', methods=("GET", "POST"))
+def edit_job(job_id):
+	c = conn.cursor()
+	if request.method == "GET":
+		job = c.execute('SELECT * from jobs WHERE job_id=?', job_id)
+		job = list(job)[0]
+		c.close()
+		return render_template('index.html', current_job=job)
+	else:
+		print(request.form)
+		c.execute('UPDATE jobs SET company=?, url=?, status=? WHERE job_id=?', (request.form['company'], request.form['url'], request.form['status'], job_id))
+		c.close()
+		return redirect('/')
