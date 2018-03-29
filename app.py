d476b882f5cc06630f2e7a1f41052e7688d74a1b
Joseph McBride <jmcbriddle@gmail.com>
2018-03-28 17:27:15 -0700

    add status change updates, notes column  and labels for inputs

--- app.py
+++ app.py
@@ -31,7 +31,6 @@ def jobs():
 			'''
 		values = (comp, url, status)
 		c = conn.cursor()
-		with c
 		q = c.execute(sql, values)
 		conn.commit()
 		c.close()
@@ -45,26 +44,37 @@ def edit_job(job_id):
 
 	c = conn.cursor()
 	if request.method == "GET":
-		job = c.execute('SELECT * FROM jobs WHERE job_id=?', job_id)
+		job = c.execute('SELECT * FROM jobs WHERE job_id=?', (job_id,))
 		job = list(job)[0]
 		c.close()
 		return render_template('index.html', current_job=job)
 	else:
 		sql = '''
-				UPDATE 
-					jobs 
-				SET 
-					company=?, url=?, status=? 
-				WHERE 
-					job_id=?
-			'''
+			UPDATE 
+				jobs 
+			SET 
+				company=?, url=?, status=?, notes=?
+			WHERE 
+				job_id=?
+		'''
 		values = (
 			request.form['company'],
 			request.form['url'], 
-			request.form['status'], 
+			request.form['status'],
+			request.form['notes'],
 			job_id,
 		)
+		status_sql = '''
+			INSERT INTO
+				status_changes(job_id, to_status, from_status)
+			VALUES
+				(?,?,?)
+		'''
+		to_status = request.form['status']
+		from_status = str(int(to_status) - 1)
+		status_values = (job_id, request.form['status'], from_status)
 		c.execute(sql, values)
+		c.execute(status_sql, status_values)
 		conn.commit()
 		c.close()
 		return redirect('/')
